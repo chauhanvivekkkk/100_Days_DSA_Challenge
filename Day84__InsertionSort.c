2aba375bc5e2f2de1781e71a4f2b24a3bd923e8c
Vivek Chuahan <chauhanvivek12203@gmail.com>
2026-04-26 19:09:47 +0530

    Pushed Day84 & 85

---
+++ Day84__InsertionSort.c
@@ -0,0 +1,53 @@
+/*
+Problem: Implement Insertion Sort - Implement the algorithm.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the sorted array
+
+Example:
+Input:
+5
+64 34 25 12 22
+
+Output:
+12 22 25 34 64
+*/
+
+#include <stdio.h>
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    insertionSort(arr, n);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
